e2f71b9cae69b9ca2279440e30856e7c1eb44d1c
Paddy Duff <paddy.duff31@googlemail.com>
2023-06-15 14:01:49 +0100

    Kevin handover

---
+++ src/StudyApi/Requests/Users/RespondToTotpMfaRequest.cs
@@ -0,0 +1,8 @@
+namespace StudyApi.Requests.Users;
+
+public class RespondToTotpMfaRequest
+{
+    public string AuthenticatorAppCode { get; set; }
+    public string SessionId { get; set; }
+    public string Username { get; set; }
+}
