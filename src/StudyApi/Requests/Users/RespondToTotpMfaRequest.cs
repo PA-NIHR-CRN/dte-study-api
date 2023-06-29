f8535f06e5af348769b3ca639f584b843778d5b4
Kevin Officer <kevin.officer@paconsulting.com>
2023-06-29 14:44:14 +0100

    token progress with verify

--- src/StudyApi/Requests/Users/RespondToTotpMfaRequest.cs
+++ src/StudyApi/Requests/Users/RespondToTotpMfaRequest.cs
@@ -3,6 +3,8 @@ namespace StudyApi.Requests.Users;
 public class RespondToTotpMfaRequest
 {
     public string AuthenticatorAppCode { get; set; }
-    public string SessionId { get; set; }
-    public string Username { get; set; }
+    public string MfaDetails { get; set; }
+
+    //public string SessionId { get; set; }
+    //public string Username { get; set; }
 }
