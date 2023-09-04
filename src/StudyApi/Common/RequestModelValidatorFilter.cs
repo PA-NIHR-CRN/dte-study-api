4e113fa10d716f7a65123ee16f43cda5b79177ba
Paddy Duff <paddy.duff31@googlemail.com>
2023-09-04 11:32:24 +0100

    added env check

--- src/StudyApi/Common/RequestModelValidatorFilter.cs
+++ src/StudyApi/Common/RequestModelValidatorFilter.cs
@@ -1,7 +1,9 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace StudyApi.Common
@@ -9,10 +11,12 @@ namespace StudyApi.Common
     public class RequestModelValidatorFilter : IAsyncActionFilter
     {
         private readonly ILogger<RequestModelValidatorFilter> _logger;
+        private readonly IWebHostEnvironment _environment;
 
-        public RequestModelValidatorFilter(ILogger<RequestModelValidatorFilter> logger)
+        public RequestModelValidatorFilter(ILogger<RequestModelValidatorFilter> logger, IWebHostEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
         }
 
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
@@ -23,14 +27,13 @@ namespace StudyApi.Common
                 var errorsString = string.Join("; ", errors);
                 _logger.LogWarning("Request Validation Failed: {Errors}", errorsString);
 
-                // Set a generic error message
-                var genericErrorResponse = new
+                // Set a generic error message if not in development
+                if (!_environment.IsDevelopment())
                 {
-                    status = 400,
-                    message = "Your request contains invalid parameters. Please check and try again."
-                };
-
-                context.Result = new BadRequestObjectResult(genericErrorResponse);
+                    errorsString = "Your request contains invalid parameters. Please check and try again.";
+                }
+                
+                context.Result = new BadRequestObjectResult(errorsString);
             }
             else
             {
