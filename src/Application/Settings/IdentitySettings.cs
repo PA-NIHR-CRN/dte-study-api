20c6fc25f194b86ab395cf396015026cebc3da21
Paddy Duff <paddy.duff31@googlemail.com>
2023-10-10 13:25:31 +0100

    Update IdentitySettings.cs

--- src/Application/Settings/IdentitySettings.cs
+++ src/Application/Settings/IdentitySettings.cs
@@ -3,7 +3,6 @@ namespace Application.Settings
     public class IdentitySettings
     {
         public static string SectionName => "IdentitySettings";
-        public string IdgExternalProviderName { get; set; }
         public string NhsLoginAuthority { get; set; }
     }
 }
