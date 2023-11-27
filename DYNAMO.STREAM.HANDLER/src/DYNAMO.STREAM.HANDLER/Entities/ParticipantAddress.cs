ea61607039f22eeca437f19b94a14c05f97d980a
Paddy Duff <paddy.duff31@googlemail.com>
2023-11-27 11:15:22 +0000

    make participant nullable for reporting

--- DYNAMO.STREAM.HANDLER/src/DYNAMO.STREAM.HANDLER/Entities/ParticipantAddress.cs
+++ DYNAMO.STREAM.HANDLER/src/DYNAMO.STREAM.HANDLER/Entities/ParticipantAddress.cs
@@ -6,12 +6,12 @@ public class ParticipantAddress
 {
     [Key] 
     public int Id { get; set; }
-    public string AddressLine1 { get; set; }
-    public string AddressLine2 { get; set; }
-    public string AddressLine3 { get; set; }
-    public string AddressLine4 { get; set; }
-    public string Town { get; set; }
-    public string Postcode { get; set; }
+    public string? AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+    public string? AddressLine3 { get; set; }
+    public string? AddressLine4 { get; set; }
+    public string? Town { get; set; }
+    public string? Postcode { get; set; }
     public int ParticipantId { get; set; }
     public Participant Participant { get; set; }
     
