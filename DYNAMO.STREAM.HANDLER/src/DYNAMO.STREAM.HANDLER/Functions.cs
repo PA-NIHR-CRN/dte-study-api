ce56202beec163be09ea3002214025f6adc7d9e3
Chris O'Neill <chris.oneill@nihr.ac.uk>
2023-12-11 13:32:14 +0000

    Removed Amazon.Lambda.Annotations

--- DYNAMO.STREAM.HANDLER/src/DYNAMO.STREAM.HANDLER/Functions.cs
+++ DYNAMO.STREAM.HANDLER/src/DYNAMO.STREAM.HANDLER/Functions.cs
@@ -1,7 +1,7 @@
-using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.DynamoDBEvents;
 using DYNAMO.STREAM.HANDLER.Handlers;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -10,20 +10,33 @@ namespace DYNAMO.STREAM.HANDLER;
 
 public class Functions
 {
-    [LambdaFunction]
-    public StreamsEventResponse ProcessStream([FromServices] IStreamHandler streamHandler,
-        [FromServices] ILogger<Functions> logger, DynamoDBEvent dynamoDbEvent)
+    private readonly ILogger<Functions> _logger;
+    private readonly IStreamHandler _streamHandler;
+
+    public Functions()
     {
-        using (logger.BeginScope("{FunctionName}", nameof(ProcessStream)))
+        var services = new ServiceCollection();
+        // Configure your services here
+        var startup = new Startup();
+        startup.ConfigureServices(services);
+        var provider = services.BuildServiceProvider();
+
+        _logger = provider.GetRequiredService<ILogger<Functions>>();
+        _streamHandler = provider.GetRequiredService<IStreamHandler>();
+    }
+
+    public StreamsEventResponse ProcessStream(DynamoDBEvent dynamoDbEvent, ILambdaContext context)
+    {
+        using (_logger.BeginScope("{FunctionName}", nameof(ProcessStream)))
         {
-            logger.LogInformation("Number of records: {RecordsCount}", dynamoDbEvent.Records.Count);
+            _logger.LogInformation("Number of records: {RecordsCount}", dynamoDbEvent.Records.Count);
 
             // AWS DynamoDb Stream handler is currently synchronous, but we want the library code
             // to remain async.
             var cts = new CancellationTokenSource();
-            var failures = streamHandler.ProcessStreamAsync(dynamoDbEvent, cts.Token).Result;
+            var failures = _streamHandler.ProcessStreamAsync(dynamoDbEvent, cts.Token).Result;
 
-            logger.LogInformation("DynamoDBEvent processing complete");
+            _logger.LogInformation("DynamoDBEvent processing complete");
 
             return new StreamsEventResponse { BatchItemFailures = failures.ToList() };
         }
