4e7128a52182bafcf1821f49838496e8bf97e385
Paddy Duff <paddy.duff31@googlemail.com>
2023-12-18 12:04:29 +0000

    revert to remove logger

--- Dynamo.Stream.Handler/src/Dynamo.Stream.Handler/Extensions/AwsSecretsConfigurationBuilderExtensions.cs
+++ Dynamo.Stream.Handler/src/Dynamo.Stream.Handler/Extensions/AwsSecretsConfigurationBuilderExtensions.cs
@@ -2,8 +2,6 @@ using Amazon.SecretsManager.Model;
 using Dte.Common.Lambda.Extensions;
 using Dynamo.Stream.Handler.Helpers;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Dynamo.Stream.Handler.Extensions;
 
@@ -11,53 +9,28 @@ public static class AwsSecretsConfigurationBuilderExtensions
 {
     private const string AwsSecretManagerSecretName = "AWS_SECRET_MANAGER_SECRET_NAME";
 
-    public static IConfigurationBuilder AddAwsSecrets(this IConfigurationBuilder configurationBuilder,
-        IServiceProvider serviceProvider)
+    public static IConfigurationBuilder AddAwsSecrets(this IConfigurationBuilder configurationBuilder)
     {
-        var logger = serviceProvider.GetService<ILoggerFactory>()!.CreateLogger("AwsSecretsConfiguration");
-        try
+        // return if in development
+        if (EnvironmentHelper.IsDevelopment())
         {
-            // Log that the method has started
-            logger.LogInformation("Starting AddAwsSecrets");
-
-            // return if in development
-            if (EnvironmentHelper.IsDevelopment())
-            {
-                logger.LogInformation("In development environment, skipping AddAwsSecrets");
-                return configurationBuilder;
-            }
-
-            var awsSecretsName = Environment.GetEnvironmentVariable(AwsSecretManagerSecretName);
-            logger.LogInformation("AWS_SECRET_MANAGER_SECRET_NAME: {AwsSecretsName}", awsSecretsName);
-
-            if (string.IsNullOrWhiteSpace(awsSecretsName))
-            {
-                logger.LogWarning("AWS secrets name is not set");
-                return configurationBuilder;
-            }
-
-            var allowedSecretNames = new[] { awsSecretsName };
-
-            // Log the secret names being added
-            logger.LogInformation($"Adding secrets for: {string.Join(", ", allowedSecretNames)}");
-
-            var secrets = configurationBuilder.AddSecretsManager(configurator: opts =>
-            {
-                opts.SecretFilter = entry => HasValue(allowedSecretNames, entry);
-                opts.KeyGenerator = (entry, key) => GenerateKey(allowedSecretNames, key);
-            });
-
-            // Log that the method has finished and the secrets
-            logger.LogInformation("Finished AddAwsSecrets, secrets: {Secrets}", string.Join(", ", secrets));
-
-            return secrets;
+            return configurationBuilder;
         }
-        catch (Exception ex)
+
+        var awsSecretsName = Environment.GetEnvironmentVariable(AwsSecretManagerSecretName);
+
+        if (string.IsNullOrWhiteSpace(awsSecretsName))
         {
-            // Log the exception
-            logger.LogError(ex, "An error occurred in AddAwsSecrets");
-            throw; // Re-throw the exception to ensure the caller knows an error occurred
+            return configurationBuilder;
         }
+
+        var allowedSecretNames = new[] { awsSecretsName };
+
+        return configurationBuilder.AddSecretsManager(configurator: opts =>
+        {
+            opts.SecretFilter = entry => HasValue(allowedSecretNames, entry);
+            opts.KeyGenerator = (entry, key) => GenerateKey(allowedSecretNames, key);
+        });
     }
 
     // Only load entries that start with any of the allowed prefixes
