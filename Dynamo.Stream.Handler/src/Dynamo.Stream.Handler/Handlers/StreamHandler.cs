72b216d0cf222b38628347dd49116b4e6c36f9ec
Paddy Duff <paddy.duff31@googlemail.com>
2023-12-18 20:48:00 +0000

    use multiple threads where possible

--- Dynamo.Stream.Handler/src/Dynamo.Stream.Handler/Handlers/StreamHandler.cs
+++ Dynamo.Stream.Handler/src/Dynamo.Stream.Handler/Handlers/StreamHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.DynamoDBEvents;
@@ -23,57 +24,42 @@ public class StreamHandler : IStreamHandler
         _participantMapper = participantMapper;
     }
 
-    public async Task<IEnumerable<BatchItemFailure>> ProcessStreamAsync(DynamoDBEvent dynamoDbEvent, CancellationToken cancellationToken)
+    public async Task<IEnumerable<BatchItemFailure>> ProcessStreamAsync(DynamoDBEvent dynamoDbEvent,
+        CancellationToken cancellationToken)
     {
-        var failures = new List<BatchItemFailure>();
-        // TODO: how do we handle out of order events if they are indeed out of order?
-        string currentRecordSequenceNumber;
-
-        foreach (var record in dynamoDbEvent.Records)
+        var failures = new ConcurrentBag<BatchItemFailure>();
+        var tasks = dynamoDbEvent.Records.Select(async record =>
         {
-            currentRecordSequenceNumber = record.Dynamodb.SequenceNumber;
-
-            using (_logger.BeginScope("{EventId}, {SequenceNumber}", record.EventID, currentRecordSequenceNumber))
+            try
             {
-                try
+                using (_logger.BeginScope("{EventId}, {SequenceNumber}", record.EventID,
+                           record.Dynamodb.SequenceNumber))
                 {
-                    _logger.LogInformation("Processing record {SequenceNumber}, ApproximateCreationDateTime: {ApproximateCreationDateTime}", currentRecordSequenceNumber, record.Dynamodb.ApproximateCreationDateTime);
-
-                    _logger.LogDebug("AwsRegion: {AwsRegion}, EventID: {EventID}, EventName: {EventName}, EventSource: {EventSource}, EventSourceArn: {EventSourceArn}, EventVersion: {EventVersion}, UserIdentity: {@UserIdentity}, SizeBytes: {SizeBytes}, StreamViewType: {StreamViewType}",
-                                           record.AwsRegion,
-                                           record.EventID,
-                                           record.EventName,
-                                           record.EventSource,
-                                           record.EventSourceArn,
-                                           record.EventVersion,
-                                           record.UserIdentity,
-                                           record.Dynamodb.SizeBytes,
-                                           record.Dynamodb.StreamViewType
-                                           );
-
+                    _logger.LogInformation(
+                        "Processing record {SequenceNumber}, ApproximateCreationDateTime: {ApproximateCreationDateTime}",
+                        record.Dynamodb.SequenceNumber, record.Dynamodb.ApproximateCreationDateTime);
                     _logger.LogTrace("{@record}", record);
 
                     await ProcessRecordAsync(record, cancellationToken);
-
                     await _dbContext.SaveChangesAsync(cancellationToken);
 
                     _logger.LogInformation("Record processing complete");
                 }
-                catch (Exception e)
-                {
-                    _logger.LogError(e, e.Message);
-                    failures.Add(new BatchItemFailure { ItemIdentifier = currentRecordSequenceNumber });
-
-                    // If there is one failure the whole batch is retried, exit early here.
-                    return failures;
-                }
             }
-        }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                failures.Add(new BatchItemFailure { ItemIdentifier = record.Dynamodb.SequenceNumber });
+            }
+        });
+
+        await Task.WhenAll(tasks);
 
         return failures;
     }
 
-    private async Task ProcessRecordAsync(DynamoDBEvent.DynamodbStreamRecord record, CancellationToken cancellationToken)
+    private async Task ProcessRecordAsync(DynamoDBEvent.DynamodbStreamRecord record,
+        CancellationToken cancellationToken)
     {
         if (record.EventName == OperationType.INSERT)
         {
@@ -93,18 +79,20 @@ public class StreamHandler : IStreamHandler
         }
     }
 
-    private async Task ProcessInsertAsync(DynamoDBEvent.DynamodbStreamRecord record, CancellationToken cancellationToken)
+    private async Task ProcessInsertAsync(DynamoDBEvent.DynamodbStreamRecord record,
+        CancellationToken cancellationToken)
     {
         await InsertAsync(record.Dynamodb.NewImage, cancellationToken);
     }
 
-    private async Task<Participant> InsertAsync(Dictionary<string, AttributeValue> image, CancellationToken cancellationToken)
+    private async Task<Participant> InsertAsync(Dictionary<string, AttributeValue> image,
+        CancellationToken cancellationToken)
     {
         var identifiers = _participantMapper.ExtractIdentifiers(image);
 
         var targetParticipant = await _dbContext.GetParticipantByLinkedIdentifiers(identifiers)
-                                                .ForUpdate()
-                                                .SingleOrDefaultAsync(cancellationToken);
+            .ForUpdate()
+            .SingleOrDefaultAsync(cancellationToken);
 
 
         if (targetParticipant == null)
@@ -116,12 +104,13 @@ public class StreamHandler : IStreamHandler
         return _participantMapper.Map(image, targetParticipant);
     }
 
-    private async Task ProcessModifyAsync(DynamoDBEvent.DynamodbStreamRecord record, CancellationToken cancellationToken)
+    private async Task ProcessModifyAsync(DynamoDBEvent.DynamodbStreamRecord record,
+        CancellationToken cancellationToken)
     {
         var identifiers = _participantMapper.ExtractIdentifiers(record.Dynamodb.NewImage);
         var participant = await _dbContext.GetParticipantByLinkedIdentifiers(identifiers)
-                                                .ForUpdate()
-                                                .SingleOrDefaultAsync(cancellationToken);
+            .ForUpdate()
+            .SingleOrDefaultAsync(cancellationToken);
 
         if (participant == null)
         {
