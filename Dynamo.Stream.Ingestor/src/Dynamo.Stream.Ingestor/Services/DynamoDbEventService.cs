6dddbdada6d7cdd02594af71026baa0c5b4f939a
Paddy Duff <paddy.duff31@googlemail.com>
2023-12-18 19:55:24 +0000

    a;tered ref data to update cache

--- Dynamo.Stream.Ingestor/src/Dynamo.Stream.Ingestor/Services/DynamoDbEventService.cs
+++ Dynamo.Stream.Ingestor/src/Dynamo.Stream.Ingestor/Services/DynamoDbEventService.cs
@@ -50,24 +50,17 @@ public class DynamoDbEventService : IDynamoDbEventService
 
     public DynamoDBEvent CreateBatchEvent(IEnumerable<Dictionary<string, AttributeValue>> participants)
     {
-        var records = new List<DynamoDBEvent.DynamodbStreamRecord>();
-
-        foreach (var participant in participants)
-        {
-            var pk = participant.PK();
-            var eventType = pk.StartsWith("DELETED#") ? OperationType.REMOVE : OperationType.INSERT;
-            var dynamoDbRecord = new DynamoDBEvent.DynamodbStreamRecord
+        var records = (from participant in participants
+            let pk = participant.PK()
+            let eventType = pk.StartsWith("DELETED#") ? OperationType.REMOVE : OperationType.INSERT
+            select new DynamoDBEvent.DynamodbStreamRecord
             {
                 EventID = Guid.NewGuid().ToString(),
                 EventName = eventType,
                 Dynamodb = new StreamRecord
                 {
                     ApproximateCreationDateTime = DateTime.UtcNow,
-                    Keys = new Dictionary<string, AttributeValue>
-                    {
-                        { "PK", participant["PK"] },
-                        { "SK", participant["SK"] }
-                    },
+                    Keys = new Dictionary<string, AttributeValue> { { "PK", participant["PK"] }, { "SK", participant["SK"] } },
                     NewImage = eventType == OperationType.INSERT
                         ? participant
                         : new Dictionary<string, AttributeValue>(),
@@ -75,16 +68,12 @@ public class DynamoDbEventService : IDynamoDbEventService
                         ? participant
                         : new Dictionary<string, AttributeValue>(),
                     SequenceNumber = Guid.NewGuid().ToString(),
-                    SizeBytes = 0, // Update as needed
                     StreamViewType = StreamViewType.NEW_AND_OLD_IMAGES,
                 },
                 EventSource = "aws:dynamodb",
                 EventVersion = "1.1",
                 AwsRegion = "eu-west-2",
-            };
-
-            records.Add(dynamoDbRecord);
-        }
+            }).ToList();
 
         return new DynamoDBEvent { Records = records };
     }
