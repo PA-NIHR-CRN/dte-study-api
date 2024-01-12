a1ee1edaf94c23d5053e6036f8e4c13a1eaed827
Paddy Duff <paddy.duff31@googlemail.com>
2024-01-12 16:32:03 +0000

    Update HealthCheckController.cs

--- src/StudyApi/Controllers/V1/HealthCheckController.cs
+++ src/StudyApi/Controllers/V1/HealthCheckController.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace StudyApi.Controllers.V1;
 
+[ApiController]
+[ApiVersion("1")]
+[Route("api/healthcheck")]
 public class HealthCheckController: Controller
 {
     [HttpGet]
-    [Route("healthcheck")]
+    [Route("")]
     [AllowAnonymous]
     public IActionResult HealthCheck()
     {
