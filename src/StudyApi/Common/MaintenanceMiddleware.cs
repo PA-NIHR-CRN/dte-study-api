378506c772ac711735a7f43622677d7f6d70efd1
Paddy Duff <paddy.duff31@googlemail.com>
2024-01-12 17:10:30 +0000

    Update MaintenanceMiddleware.cs

--- src/StudyApi/Common/MaintenanceMiddleware.cs
+++ src/StudyApi/Common/MaintenanceMiddleware.cs
@@ -8,16 +8,16 @@ namespace StudyApi.Common;
 public class MaintenanceMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly DevSettings _devSettings;
+    private readonly IOptionsSnapshot<DevSettings> _devSettings;
     public MaintenanceMiddleware(IOptionsSnapshot<DevSettings> devSettings, RequestDelegate next)
     {
-        _devSettings = devSettings.Value;
+        _devSettings = devSettings;
         _next = next;
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
-        if (_devSettings.IsInMaintenance)
+        if (_devSettings.Value.IsInMaintenance)
         {
             context.Response.StatusCode = 503;
             await context.Response.WriteAsync("Service is in maintenance mode");
