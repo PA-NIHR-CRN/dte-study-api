006907c34461cf74d65dccf3a4c54f8d99f1af05
Chris O'Neill <chris.oneill@nihr.ac.uk>
2024-01-22 12:04:41 +0000

    Add missing files

---
+++ Dynamo.Stream.Handler/src/Dynamo.Stream.Handler/Entities/Configuration/SysConfigurationConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Dynamo.Stream.Handler.Entities.System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dynamo.Stream.Handler.Entities.Configuration
+{
+    public class SysConfigurationConfiguration : IEntityTypeConfiguration<SysConfiguration>
+    {
+        public enum ConfigurationKeys
+        {
+            IsInMaintenanceMode,
+        }
+
+        public void Configure(EntityTypeBuilder<SysConfiguration> builder)
+        {
+            builder.ToTable("SysConfiguration");
+
+            builder.HasData(new SysConfiguration { Id = 1, Name = nameof(ConfigurationKeys.IsInMaintenanceMode), Value = false.ToString() });
+        }
+    }
+}
---
+++ Dynamo.Stream.Handler/src/Dynamo.Stream.Handler/Migrations/20240122115245_Add SysConfiguration table.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dynamo.Stream.Handler.Migrations
+{
+    public partial class AddSysConfigurationtable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SysConfiguration",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Value = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SysConfiguration", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "SysConfiguration",
+                columns: new[] { "Id", "Name", "Value" },
+                values: new object[] { 1, "IsInMaintenanceMode", "False" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SysConfiguration");
+        }
+    }
+}
