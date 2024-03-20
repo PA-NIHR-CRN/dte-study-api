2ad02c107a93aca51f719c8df33aba46c351a92d
Paddy Duff <paddy.duff31@googlemail.com>
2024-03-20 15:16:09 +0000

    initial view and login

---
+++ BPOR/BPOR.Rms/wwwroot/js/layout.js
@@ -0,0 +1,6 @@
+﻿function toggleNavigation() {
+  const iconContainer = document.querySelector('.icon-container');
+  const sideNav = document.querySelector('#nihr-side-nav');
+  iconContainer.classList.toggle('clicked');
+  sideNav.classList.toggle('show');
+}
