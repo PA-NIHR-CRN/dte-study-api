ec818afa74046d243ee2395612f6c4a7aa77c5ef
Paddy Duff <paddy.duff31@googlemail.com>
2024-04-16 13:51:01 +0100

    add view model to my get request

--- BPOR/BPOR.Rms/Controllers/EmailController.cs
+++ BPOR/BPOR.Rms/Controllers/EmailController.cs
@@ -1,29 +1,105 @@
+using BPOR.Rms.Models;
 using BPOR.Rms.Models.Email;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BPOR.Rms.Controllers;
 
 public class EmailController : Controller
 {
-    public Task<IActionResult> SetupCampaign()
+    public IActionResult SetupCampaign(SetupCampaignViewModel model)
     {
-        return Task.FromResult<IActionResult>(View());
+        if (TempData["Notification"] != null)
+        {
+            model.Notification =
+                JsonConvert.DeserializeObject<NotificationBannerModel>(TempData["Notification"].ToString());
+        }
+
+        return View(model);
     }
 
     [HttpPost]
-    public IActionResult SetupCampaign(SetupCampaignViewModel model)
+    public IActionResult SetupCampaign()
     {
-        return View(model);
-        }
+        return View();
+    }
+
     [HttpPost]
     public IActionResult SendEmail(SetupCampaignViewModel model)
-        {
+    {
         return RedirectToAction("EmailSuccess", model);
     }
 
-    
+
     public Task<IActionResult> EmailSuccess(EmailSuccessViewModel model)
     {
         return Task.FromResult<IActionResult>(View(model));
+    }
+
+    public IActionResult SendPreviewEmail(SetupCampaignViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("EmailSuccess", model);
+        }
+
+        if (model.PreviewEmails != null)
+        {
+            var emails = model.PreviewEmails.Split(new[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var email in emails)
+            {
+                if (!IsValidEmail(email))
+                {
+                    ModelState.AddModelError("PreviewEmails", "Please enter a valid email address.");
+                    break;
+                }
+            }
+        }
+
+        ModelState.Remove("MaxNumbers");
+        ModelState.Remove("TotalVolunteers");
+        ModelState.Remove("StudyName");
+        ModelState.Remove("SelectedTemplate");
+
+        TempData["Notification"] = JsonConvert.SerializeObject(new NotificationBannerModel
+        {
+            IsSuccess = true,
+            Heading = "Success",
+            Body = $"Preview email has been sent to{model.PreviewEmails}"
+        });
+
+        return RedirectToAction("SetupCampaign", model);
+    }
+
+
+    [HttpPost]
+    public IActionResult HandleForms(SetupCampaignViewModel model, string action)
+    {
+        switch (action)
+        {
+            case "SetupCampaign":
+                return RedirectToAction("SetupCampaign");
+                break;
+            case "SendPreviewEmail":
+                return SendPreviewEmail(model);
+                break;
+            default:
+                break;
+        }
+
+        return RedirectToAction("SetupCampaign");
+    }
+
+    private bool IsValidEmail(string email)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
--- BPOR/BPOR.Rms/Models/Email/SetupCampaignViewModel.cs
+++ BPOR/BPOR.Rms/Models/Email/SetupCampaignViewModel.cs
@@ -5,15 +5,22 @@ namespace BPOR.Rms.Models.Email;
 
 public class SetupCampaignViewModel
 {
+    public int? StudyId { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "Please enter a number greater than zero for Max Numbers.")]
     public int MaxNumbers { get; set; }
-    
+
     [DisplayName("How many volunteers do you want to send it to?")]
+    [Range(1, int.MaxValue, ErrorMessage = "Total Volunteers must be at least 1.")]
     public int TotalVolunteers { get; set; }
-    public string StudyName { get; set; }
-    
-    [DisplayName("Select email template")]
+
+    public string? StudyName { get; set; }
+
     public string SelectedTemplate { get; set; }
-    
+
     [DisplayName("Preview email")]
+    [Required(ErrorMessage = "Email address is required.")]
+    [EmailAddress(ErrorMessage = "Enter a valid email address.")]
     public string PreviewEmails { get; set; }
+    public NotificationBannerModel? Notification { get; set; }
 }
