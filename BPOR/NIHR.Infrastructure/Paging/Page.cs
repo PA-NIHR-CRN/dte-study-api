cf7609d8002e01202f1fb15417b14a9a85a920e5
Chris O'Neill <chris.oneill@nihr.ac.uk>
2024-04-22 15:24:39 +0100

    Make Page abstract, non-generic IEnumerable. Expose IPage interface.

--- BPOR/NIHR.Infrastructure/Paging/Page.cs
+++ BPOR/NIHR.Infrastructure/Paging/Page.cs
@@ -1,34 +1,35 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NIHR.Infrastructure.Paging
 {
-    public class Page<T> : Page, IEnumerable<T>, IEnumerable
+    public class Page<T> : Page, IEnumerable<T>
     {
         private ICollection<T> _items;
 
-        public Page(ICollection<T> source, int pageSize, int currentPage, long totalCount) : base(pageSize, currentPage, totalCount)   
+        public Page(ICollection<T> source, int pageSize, int currentPage, long totalCount) : base(source, pageSize, currentPage, totalCount)   
         {
             _items = source;
         }
 
         public ICollection<T> Items { get => _items; }
 
-        public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => _items.GetEnumerator();
+        public new IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
     }
 
-    public class Page
+    public abstract class Page : IPage
     {
         private long _totalCount;
+        private IEnumerable _items;
         private int _pageSize;
         private int _currentPage;
 
-        public Page(int pageSize, int currentPage, long totalCount)
+        public Page(IEnumerable source, int pageSize, int currentPage, long totalCount)
         {
             _totalCount = totalCount;
+            _items = source;
             _pageSize = pageSize;
             _currentPage = currentPage;
         }
@@ -40,5 +41,20 @@ namespace NIHR.Infrastructure.Paging
         public long TotalPages => (long)Math.Ceiling((double)TotalCount / PageSize);
         public bool HasPreviousPage => CurrentPage > 1;
         public bool HasNextPage => CurrentPage < TotalPages;
+
+        public int CurrentCount() => _items.Cast<object>().Count();
+
+        public IEnumerator GetEnumerator() => _items.GetEnumerator();
+    }
+
+    public interface IPage : IEnumerable
+    {
+        public int CurrentPage { get; }
+        public int CurrentCount();
+        public int PageSize { get; }
+        public long TotalCount { get; }
+        public long TotalPages { get; }
+        public bool HasPreviousPage { get; }
+        public bool HasNextPage { get; }
     }
 }
