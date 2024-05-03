b8784a07e7517e54cb80b1486559abe34f88b731
Chris O'Neill <chris.oneill@nihr.ac.uk>
2024-05-03 16:14:57 +0100

    Make option search and reusable control.

---
+++ BPOR/BPOR.Rms/wwwroot/js/nihr-ui.js
@@ -0,0 +1,27 @@
+﻿document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        initialiseOptionSearch();
+    }
+}
+
+function initialiseOptionSearch() {
+    Array.from(document.getElementsByClassName('nihr-option-search')).forEach(function (search) {
+        search.classList.remove("js-only");
+        search.style.display = 'unset';
+
+        search.addEventListener('input', function () {
+            var searchText = this.value.toLowerCase();
+            var optionClass = this.dataset.optionClass;
+            var checkboxes = document.querySelectorAll('.' + optionClass);
+
+            checkboxes.forEach(function (checkbox) {
+                var label = checkbox.dataset.searchValue;
+                if (label.includes(searchText)) {
+                    checkbox.style.display = 'flex';
+                } else {
+                    checkbox.style.display = 'none';
+                }
+            });
+        });
+    });
+}
