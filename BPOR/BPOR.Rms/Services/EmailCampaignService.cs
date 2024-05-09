c8ee2512969b1fc1fc9de50263da7f795aecbcee
PAULTENNYSOA361\paul.tennyson <paul.tennyson@paconsulting.com>
2024-05-09 15:29:42 +0100

    Send email - work in progress

--- BPOR/BPOR.Rms/Services/EmailCampaignService.cs
+++ BPOR/BPOR.Rms/Services/EmailCampaignService.cs
@@ -2,6 +2,8 @@ using BPOR.Domain.Entities;
 using BPOR.Registration.Stream.Handler.Services;
 using BPOR.Rms.Constants;
 using BPOR.Rms.Mappers;
+using BPOR.Rms.Models.Study;
+using Microsoft.EntityFrameworkCore;
 using NIHR.Infrastructure.Interfaces;
 using NIHR.Infrastructure.Models;
 
@@ -15,7 +17,7 @@ public class EmailCampaignService(
     IRandomiser randomiser)
     : IEmailCampaignService
 {
-    public async Task SendCampaignAsync(EmailCampaign campaign, CancellationToken cancellationToken = default)
+    public async Task SendCampaignAsync(Domain.Entities.EmailCampaign campaign, CancellationToken cancellationToken = default)
     {
         try
         {
@@ -27,37 +29,92 @@ public class EmailCampaignService(
             {
                 var filter = FilterMapper.MapToFilterModel(dbFilter);
                 var volunteers = await filterService.FilterVolunteersAsync(filter, cancellationToken);
+                var participantIds = volunteers.Select(v => v.Id).ToList();
+                List<Participant> finalVolunteers = new List<Participant>();
 
-                // TODO implement this logic
-                // prioritise volunteers who have not been contacted before or not contacted in a while
-                var prioritisedVolunteers = volunteers;
-
-                // Randomise the order of the volunteers
                 if (campaign.TargetGroupSize != null)
                 {
-                    var selectedVolunteers =
-                        randomiser.GetRandomisedCollection(prioritisedVolunteers, campaign.TargetGroupSize.Value);
+                    List<string?> emailAddresses = new List<string?>();
 
-                    var emailAddresses = selectedVolunteers.Select(v => v.Email).ToList();
+                    var participantsNotContacted = participantIds.Where(participantId =>
+                                                        !context.EmailCampaignParticipants.Any(ecp =>
+                                                            ecp.ParticipantId == participantId &&
+                                                            context.EmailCampaigns.Any(ec =>
+                                                                ec.Id == ecp.EmailCampaignId &&
+                                                                context.FilterCriterias.Any(fc =>
+                                                                    fc.Id == ec.FilterCriteriaId &&
+                                                                    fc.StudyId != null))));
+
+                    // Priortise those who have not been contacted, randomise the remaining volunteers
+                    if (participantsNotContacted.Count() < campaign.TargetGroupSize.Value)
+                    {
+                        var prioritisedVolunteers = volunteers.Where(v => participantsNotContacted.Contains(v.Id));
+                        var volunteersToRandomise = volunteers.Where(v => !participantsNotContacted.Contains(v.Id));
+                        var randomisedVolunteers = randomiser.GetRandomisedCollection(volunteersToRandomise, campaign.TargetGroupSize.Value - prioritisedVolunteers.Count());
+                        var selectedVolunteers = randomisedVolunteers.Concat(prioritisedVolunteers);
+                        emailAddresses = selectedVolunteers.Select(v => v.Email).ToList();
+                        finalVolunteers = selectedVolunteers.ToList();
+                    }
+                    // If total participants not contacted exceeds target group size, randomise particpants not contacted based on target group size
+                    else if (participantsNotContacted.Count() > campaign.TargetGroupSize.Value)
+                    {
+                        var prioritisedVolunteers = volunteers.Where(v => participantsNotContacted.Contains(v.Id));
+                        var selectedVolunteers = randomiser.GetRandomisedCollection(prioritisedVolunteers, campaign.TargetGroupSize.Value);
+                        emailAddresses = selectedVolunteers.Select(v => v.Email).ToList();
+                        finalVolunteers = selectedVolunteers.ToList();
+                    }
+                    // If total participants not contacted matches target group size, send email to those not contacted and do not randomise
+                    else if (participantsNotContacted.Count() == campaign.TargetGroupSize.Value)
+                    {
+                        var selectedVolunteers = volunteers.Where(v => participantsNotContacted.Contains(v.Id));
+                        emailAddresses = selectedVolunteers.Select(v => v.Email).ToList();
+                        finalVolunteers = selectedVolunteers.ToList();
+                    }
 
                     await notificationService.SendBatchEmailAsync(new SendBatchEmailRequest
                     {
                         EmailAddresses = emailAddresses,
                         EmailTemplateId = campaign.EmailTemplateId,
                     }, cancellationToken);
-            
-                    var sentAt = DateTime.UtcNow;
 
-                    foreach (var volunteer in selectedVolunteers)
+                    var studyId = context.FilterCriterias.Where(fc => fc.Id == campaign.FilterCriteriaId).Select(fc => fc.StudyId).FirstOrDefault();
+                    var isStudyRecruitingIdentifiable = context.Studies.Where(s => s.Id == studyId).Select(s => s.IsRecruitingIdentifiableParticipants).FirstOrDefault();
+
+                    if (!isStudyRecruitingIdentifiable)
                     {
-                        context.EmailCampaignParticipants.Add(new EmailCampaignParticipant
+                        foreach (var volunteer in finalVolunteers)
                         {
-                            EmailCampaignId = campaign.Id,
-                            ParticipantId = volunteer.Id,
-                            DeliveryStatusId = refDataService.GetEmailDeliveryStatusId(EmailDeliveryStatus.Pending),
-                            SentAt = sentAt,
-                            ContactEmail = volunteer.Email,
-                        });
+                            context.EmailCampaignParticipants.Add(new Domain.Entities.EmailCampaignParticipant
+                            {
+                                EmailCampaignId = campaign.Id,
+                                ParticipantId = volunteer.Id,
+                                DeliveryStatusId = refDataService.GetEmailDeliveryStatusId(EmailDeliveryStatus.Pending),
+                                SentAt = DateTime.UtcNow,
+                                ContactEmail = volunteer.Email,
+                            });
+                        }
+                    }
+                    else if (isStudyRecruitingIdentifiable && studyId != null)
+                    {
+                        foreach (var volunteer in finalVolunteers)
+                        {
+                            context.EmailCampaignParticipants.Add(new Domain.Entities.EmailCampaignParticipant
+                            {
+                                EmailCampaignId = campaign.Id,
+                                ParticipantId = volunteer.Id,
+                                DeliveryStatusId = refDataService.GetEmailDeliveryStatusId(EmailDeliveryStatus.Pending),
+                                SentAt = DateTime.UtcNow,
+                                ContactEmail = volunteer.Email,
+                            });
+
+                            context.StudyParticipantEnrollment.Add(new StudyParticipantEnrollment
+                            {
+                                StudyId = studyId.Value,
+                                ParticipantId = volunteer.Id,
+                                Reference = "1234" // TODO - generate ref number using algorithm
+
+                            });
+                        }
                     }
                 }
             }
