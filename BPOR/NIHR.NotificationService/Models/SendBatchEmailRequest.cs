611b081713923775692e5485ff2d1ac96bdc3615
Paddy Duff <paddy.duff31@googlemail.com>
2024-05-16 16:15:12 +0100

    fix merge conflicts

--- BPOR/NIHR.NotificationService/Models/SendBatchEmailRequest.cs
+++ BPOR/NIHR.NotificationService/Models/SendBatchEmailRequest.cs
@@ -2,7 +2,7 @@ namespace NIHR.NotificationService.Models
 {
     public class SendBatchEmailRequest
     {
-        public ICollection<string> EmailAddresses { get; set; }
+        public IEnumerable<string> EmailAddresses { get; set; }
         public Dictionary<string, Dictionary<string, dynamic>> PersonalisationData { get; set; }
         public Guid? EmailTemplateId { get; set; }
     }
