82727291bc96476a9a5c77cddf570566aa542a56
Chris O'Neill <chris.oneill@nihr.ac.uk>
2024-05-28 11:28:22 +0100

    Use partial tag helper over HTML helper. Add thousands separator formatting to volunteer count. Created custom pagination tag helper. Replaced Breadcrumbs component with Breadcrumbs partial (no server side code necessary). Removed redundant styles from _StudyListHeader.

--- BPOR/BPOR.Rms/Views/ViewComponents/BreadcrumbsViewComponent.cs
+++ BPOR/BPOR.Rms/Views/ViewComponents/BreadcrumbsViewComponent.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace BPOR.Rms.Views.ViewComponents;
@@ -7,14 +7,6 @@ public class BreadcrumbItem(string text, string url)
 {
     public string Text { get; set; } = text;
     public string Url { get; set; } = url;
-}
-
-public class BreadcrumbsViewComponent : ViewComponent
-{
-    public IViewComponentResult Invoke()
-    {
-        return View();
-    }
 }
 
 public static class BreadcrumbExtensions
