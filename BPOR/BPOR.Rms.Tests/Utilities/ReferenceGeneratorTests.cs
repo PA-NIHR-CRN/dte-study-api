d8ed7df4a1478c3a05a1bf0dab024079624a0d68
Paddy Duff <paddy.duff31@googlemail.com>
2024-05-28 11:34:48 +0100

    updated to use interface

--- BPOR/BPOR.Rms.Tests/Utilities/ReferenceGeneratorTests.cs
+++ BPOR/BPOR.Rms.Tests/Utilities/ReferenceGeneratorTests.cs
@@ -1,12 +1,13 @@
 using System.Text.RegularExpressions;
 using BPOR.Rms.Utilities;
+using BPOR.Rms.Utilities.Interfaces;
 using LuhnNet;
 
 namespace BPOR.Rms.Tests.Utilities;
 
 public partial class ReferenceGeneratorTests
 {
-    private readonly ReferenceGenerator _generator = new();
+    private readonly IReferenceGenerator _generator = new ReferenceGenerator();
     private const int _expectedLength = 16;
     private static readonly Regex _digitsOnlyRegex = MyRegex();
 
@@ -41,6 +42,23 @@ public partial class ReferenceGeneratorTests
 
         // Assert
         Assert.Equal(checkDigit, Luhn.CalculateCheckDigit(string.Concat(withoutCheckDigit)));
+    }
+    
+    [Fact]
+    public void GenerateReference_ShouldBeUnique()
+    {
+        // Arrange
+        var references = new HashSet<string>();
+
+        // Act
+        for (var i = 0; i < 1000; i++)
+        {
+            var reference = _generator.GenerateReference();
+            references.Add(reference);
+        }
+
+        // Assert
+        Assert.Equal(1000, references.Count);
     }
 
     [GeneratedRegex(@"^\d+$")]
