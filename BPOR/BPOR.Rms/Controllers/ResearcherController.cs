59fae685894aa2ba5024ecb6e80f6ba10dc5b0f9
PAULTENNYSOA361\paul.tennyson <paul.tennyson@paconsulting.com>
2024-05-29 16:03:43 +0100

    Add study steps 1-5

--- BPOR/BPOR.Rms/Controllers/ResearcherController.cs
+++ BPOR/BPOR.Rms/Controllers/ResearcherController.cs
@@ -1,10 +1,12 @@
+using BPOR.Domain.Entities;
 using BPOR.Rms.Models.Email;
 using BPOR.Rms.Models.Researcher;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BPOR.Rms.Controllers;
 
-public class ResearcherController: Controller
+public class ResearcherController(ParticipantDbContext context) : Controller
 {
     public IActionResult Create()
     {
@@ -31,18 +33,146 @@ public class ResearcherController: Controller
         return View(model);
     }
 
-    public IActionResult CreateStudy()
+    public IActionResult TermsAndConditions()
     {
-        return View(new ResearcherStudyFormViewModel());
+        return View(new ResearcherTermsAndConditionsViewModel());
     }
 
     [HttpPost]
-    public IActionResult CreateStudy(ResearcherStudyFormViewModel model)
+    public IActionResult TermsAndConditions(ResearcherTermsAndConditionsViewModel model)
     {
         if (!model.AgreedToTermsAndConditions)
         {
             ModelState.AddModelError("AgreedToTermsAndConditions", "Confirm that the terms and conditions have been read and agreed before applying");
         }
+
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("SubmitStudy", model);
+        }
+
+        return View(model);
+    }
+
+    public IActionResult SubmitStudy(ResearcherStudyFormViewModel model)
+    {
+        ViewData["ShowBackLink"] = true;
+        ViewData["ShowProgressBar"] = true;
+        ViewData["ProgressPercentage"] = 0;
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult SubmitStudy(ResearcherStudyFormViewModel model, string action)
+    {
+        ViewData["ShowBackLink"] = true;
+        ViewData["ShowProgressBar"] = true;
+        ViewData["ProgressPercentage"] = (model.Step - 1) * 50;
+
+        model.PortfolioSubmissionStatusOptions = context.Submitted.ToList();
+        model.OutcomeOfSubmissionOptions = context.SubmissionOutcome.ToList();
+
+        if (action == "Next" && model.Step == 1)
+        {
+            if (String.IsNullOrEmpty(model.ShortName))
+            {
+                ModelState.AddModelError("ShortName", "Enter the study short name");
+            }
+
+            if (String.IsNullOrEmpty(model.ChiefInvestigator))
+            {
+                ModelState.AddModelError("ChiefInvestigator", "Enter the name of the Chief Investigator for the study");
+            }
+
+            if (String.IsNullOrEmpty(model.StudySponsors))
+            {
+                ModelState.AddModelError("StudySponsors", "Enter the name(s) of the study sponsor(s), funder(s) and CRO (if applicable)");
+            }
+
+            if (ModelState.IsValid)
+            {
+                model.Step = 2;
+                ViewData["ProgressPercentage"] = (model.Step - 1) * 50;
+                return View(model);
+            }
+        }
+        else if (action == "Next" && model.Step == 2)
+        {
+            if (model.PortfolioSubmissionStatus == null)
+            {
+                ModelState.AddModelError("PortfolioSubmissionStatus", "Select whether the study has been submitted for inclusion on the NIHR CRN portfolio");
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (model.PortfolioSubmissionStatus == 1)
+                {
+                    model.Step = 3;
+                }
+                else
+                {
+                    model.Step = 4;
+                }
+                ViewData["ProgressPercentage"] = (model.Step - 1) * 50;
+                return View(model);
+            }
+        }
+        else if (action == "Next" && model.Step == 3)
+        {
+            if (model.OutcomeOfSubmission == null)
+            {
+                ModelState.AddModelError("OutcomeOfSubmission", "Select the outcome of the submission for inclusion on the NIHR CRN portfolio");
+            }
+
+            if (model.CPMSId == null)
+            {
+                ModelState.AddModelError("CPMSId", "Enter the CPMS ID for the study");
+            }
+
+            if (ModelState.IsValid)
+            {
+                model.Step = 4;
+                ViewData["ProgressPercentage"] = (model.Step - 1) * 50;
+                return View(model);
+            }
+        }
+        else if (action == "Next" && model.Step == 4)
+        {
+            if (model.HasFunding == null)
+            {
+                ModelState.AddModelError("HasFunding", "Select whether the study has NIHR funding");
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (model.HasFunding == true)
+                {
+                    model.Step = 5;
+                }
+                else
+                {
+                    model.Step = 6;
+                }
+                ViewData["ProgressPercentage"] = (model.Step - 1) * 50;
+                return View(model);
+            }
+        }
+        else if (action == "Next" && model.Step == 5)
+        {
+            if (String.IsNullOrEmpty(model.FundingCode))
+            {
+                ModelState.AddModelError("FundingCode", "Enter the NIHR funding stream or grant code");
+            }
+
+            if (ModelState.IsValid)
+            {
+                model.Step = 6;
+                ViewData["ProgressPercentage"] = (model.Step - 1) * 50;
+                return View(model);
+            }
+        }
+
         return View(model);
     }
 
--- BPOR/BPOR.Rms/Models/Researcher/ResearcherStudyFormViewModel.cs
+++ BPOR/BPOR.Rms/Models/Researcher/ResearcherStudyFormViewModel.cs
@@ -1,10 +1,39 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using BPOR.Domain.Entities.RefData;
+using System.ComponentModel.DataAnnotations;
 
 namespace BPOR.Rms.Models.Researcher
 {
     public class ResearcherStudyFormViewModel
     {
-        public bool AgreedToTermsAndConditions { get; set; }
+        public int Step { get; set; } = 1;
+        public int TotalSteps { get; set; } = 9;
 
+        [Display(Name = "What is the study short name?")]
+        public string? ShortName { get; set; }
+
+        [Display(Name = "Who is the Chief Investigator for the study?")]
+        public string? ChiefInvestigator { get; set; }
+
+        [Display(Name = "Provide the name(s) of the study sponsor(s), funder(s) and CRO (if applicable)")]
+        public string? StudySponsors { get; set; }
+
+        public List<Submitted>? PortfolioSubmissionStatusOptions { get; set; }
+
+        [Display(Name = "Has the study been submitted for inclusion on the NIHR CRN portfolio?")]
+        public int? PortfolioSubmissionStatus { get; set; }
+
+        public List<SubmissionOutcome>? OutcomeOfSubmissionOptions { get; set; }
+
+        [Display(Name = "Outcome of submission")]
+        public int? OutcomeOfSubmission { get; set; }
+
+        [Display(Name = "CPMS ID")]
+        public string? CPMSId { get; set; }
+
+        [Display(Name = "Does the study have NIHR funding?")]
+        public bool? HasFunding { get; set; }
+
+        [Display(Name = "NIHR funding stream or grant code")]
+        public string? FundingCode { get; set; }
     }
 }
