4a78d2de78ff906e83b6e0339d2c74a237e3f13f
PAULTENNYSOA361\paul.tennyson <paul.tennyson@paconsulting.com>
2024-06-21 11:28:49 +0100

    Validate study fields after check your answers

--- BPOR/BPOR.Rms/Controllers/ResearcherController.cs
+++ BPOR/BPOR.Rms/Controllers/ResearcherController.cs
@@ -76,9 +76,13 @@ public class ResearcherController(ParticipantDbContext context, ICurrentUserProv
 
         if (action == "RedirectToCheckAnswers")
         {
-            model.Step = 8;
-            ViewData["ShowProgressBar"] = false;
-            model.RedirectToCheckYourAnswers = false;
+            ValidateMandatoryFields(model);
+            if (ModelState.IsValid)
+            {
+                model.Step = 8;
+                ViewData["ShowProgressBar"] = false;
+                model.RedirectToCheckYourAnswers = false;
+            }
             return View(model);
         }
 
@@ -119,20 +123,7 @@ public class ResearcherController(ParticipantDbContext context, ICurrentUserProv
 
         if ((action == "Next" && model.Step == 1) || action == "RedirectStep2")
         {
-            if (String.IsNullOrEmpty(model.ShortName))
-            {
-                ModelState.AddModelError("ShortName", "Enter the study short name");
-            }
-
-            if (String.IsNullOrEmpty(model.ChiefInvestigator))
-            {
-                ModelState.AddModelError("ChiefInvestigator", "Enter the name of the Chief Investigator for the study");
-            }
-
-            if (String.IsNullOrEmpty(model.StudySponsors))
-            {
-                ModelState.AddModelError("StudySponsors", "Enter the name(s) of the study sponsor(s), funder(s) and CRO (if applicable)");
-            }
+            ValidateMandatoryFields(model);
 
             if (ModelState.IsValid)
             {
@@ -143,10 +134,7 @@ public class ResearcherController(ParticipantDbContext context, ICurrentUserProv
         }
         else if ((action == "Next" && model.Step == 2) || action == "RedirectStep3")
         {
-            if (model.PortfolioSubmissionStatus == null)
-            {
-                ModelState.AddModelError("PortfolioSubmissionStatus", "Select whether the study has been submitted for inclusion on the NIHR CRN portfolio");
-            }
+            ValidateMandatoryFields(model);
 
             if (ModelState.IsValid)
             {
@@ -174,15 +162,7 @@ public class ResearcherController(ParticipantDbContext context, ICurrentUserProv
         }
         else if ((action == "Next" && model.Step == 3) || action == "RedirectStep4")
         {
-            if (model.OutcomeOfSubmission == null && model.PortfolioSubmissionStatus == 1)
-            {
-                ModelState.AddModelError("OutcomeOfSubmission", "Select the outcome of the submission for inclusion on the NIHR CRN portfolio");
-            }
-
-            if (model.CPMSId == null && model.PortfolioSubmissionStatus == 1)
-            {
-                ModelState.AddModelError("CPMSId", "Enter the CPMS ID for the study");
-            }
+            ValidateMandatoryFields(model);
 
             if (ModelState.IsValid)
             {
@@ -193,10 +173,7 @@ public class ResearcherController(ParticipantDbContext context, ICurrentUserProv
         }
         else if ((action == "Next" && model.Step == 4) || action == "RedirectStep5")
         {
-            if (model.HasFunding == null)
-            {
-                ModelState.AddModelError("HasFunding", "Select whether the study has NIHR funding");
-            }
+            ValidateMandatoryFields(model);
 
             if (ModelState.IsValid)
             {
@@ -222,10 +199,7 @@ public class ResearcherController(ParticipantDbContext context, ICurrentUserProv
         }
         else if ((action == "Next" && model.Step == 5) || action == "RedirectStep6")
         {
-            if (String.IsNullOrEmpty(model.FundingCode) && model.HasFunding == true)
-            {
-                ModelState.AddModelError("FundingCode", "Enter the NIHR funding stream or grant code");
-            }
+            ValidateMandatoryFields(model);
 
             if (ModelState.IsValid)
             {
@@ -236,15 +210,7 @@ public class ResearcherController(ParticipantDbContext context, ICurrentUserProv
         }
         else if ((action == "Next" && model.Step == 6) || action == "RedirectStep7")
         {
-            if (model.UKRecruitmentTarget == null)
-            {
-                ModelState.AddModelError("UKRecruitmentTarget", "Enter the UK recruitment target for the study");
-            }
-
-            if (String.IsNullOrEmpty(model.TargetPopulation))
-            {
-                ModelState.AddModelError("TargetPopulation", "Enter the target population for the study");
-            }
+            ValidateMandatoryFields(model);
 
             if (ModelState.IsValid)
             {
@@ -255,31 +221,7 @@ public class ResearcherController(ParticipantDbContext context, ICurrentUserProv
         }
         else if (action == "Next" && model.Step == 7)
         {
-            ValidateRecruitmentDates(model);
-
-            if (ModelState["RecruitmentStartDate.Day"].Errors.Count == 0 &&
-                ModelState["RecruitmentStartDate.Month"].Errors.Count == 0 &&
-                ModelState["RecruitmentStartDate.Year"].Errors.Count == 0 &&
-                ModelState["RecruitmentEndDate.Day"].Errors.Count == 0 &&
-                ModelState["RecruitmentEndDate.Month"].Errors.Count == 0 &&
-                ModelState["RecruitmentEndDate.Year"].Errors.Count == 0)
-            {
-                DateOnly today = DateOnly.FromDateTime(DateTime.Now);
-                if (model.RecruitmentEndDate.ToDateOnly() < today)
-                {
-                    ModelState.AddModelError("RecruitmentEndDate.Day", "Recruitment end date (UK) must be in the future");
-                }
-
-                if (model.RecruitmentStartDate.ToDateOnly() > model.RecruitmentEndDate.ToDateOnly())
-                {
-                    ModelState.AddModelError("RecruitmentEndDate.Day", "Recruitment end date (UK) must be the same as or after Recruitment start date (UK)");
-                }
-            }
-
-            if (model.RecruitingIdentifiableVolunteers == null)
-            {
-                ModelState.AddModelError("RecruitingIdentifiableVolunteers", "Select whether participants in the study will be recruited as named individual volunteers");
-            }
+            ValidateMandatoryFields(model);
 
             if (ModelState.IsValid)
             {
@@ -336,6 +278,106 @@ public class ResearcherController(ParticipantDbContext context, ICurrentUserProv
         }
 
         return View(model);
+    }
+
+    private void ValidateMandatoryFields(ResearcherStudyFormViewModel model)
+    {
+        if (model.Step == 1)
+        {
+            if (String.IsNullOrEmpty(model.ShortName))
+            {
+                ModelState.AddModelError("ShortName", "Enter the study short name");
+            }
+
+            if (String.IsNullOrEmpty(model.ChiefInvestigator))
+            {
+                ModelState.AddModelError("ChiefInvestigator", "Enter the name of the Chief Investigator for the study");
+            }
+
+            if (String.IsNullOrEmpty(model.StudySponsors))
+            {
+                ModelState.AddModelError("StudySponsors", "Enter the name(s) of the study sponsor(s), funder(s) and CRO (if applicable)");
+            }
+        }
+
+        if (model.Step == 2)
+        {
+            if (model.PortfolioSubmissionStatus == null)
+            {
+                ModelState.AddModelError("PortfolioSubmissionStatus", "Select whether the study has been submitted for inclusion on the NIHR CRN portfolio");
+            }
+        }
+
+        if (model.Step == 3)
+        {
+            if (model.OutcomeOfSubmission == null && model.PortfolioSubmissionStatus == 1)
+            {
+                ModelState.AddModelError("OutcomeOfSubmission", "Select the outcome of the submission for inclusion on the NIHR CRN portfolio");
+            }
+
+            if (model.CPMSId == null && model.PortfolioSubmissionStatus == 1)
+            {
+                ModelState.AddModelError("CPMSId", "Enter the CPMS ID for the study");
+            }
+        }
+
+        if (model.Step == 4)
+        {
+            if (model.HasFunding == null)
+            {
+                ModelState.AddModelError("HasFunding", "Select whether the study has NIHR funding");
+            }
+        }
+
+        if (model.Step == 5)
+        {
+            if (String.IsNullOrEmpty(model.FundingCode) && model.HasFunding == true)
+            {
+                ModelState.AddModelError("FundingCode", "Enter the NIHR funding stream or grant code");
+            }
+        }
+
+        if (model.Step == 6)
+        {
+            if (model.UKRecruitmentTarget == null)
+            {
+                ModelState.AddModelError("UKRecruitmentTarget", "Enter the UK recruitment target for the study");
+            }
+
+            if (String.IsNullOrEmpty(model.TargetPopulation))
+            {
+                ModelState.AddModelError("TargetPopulation", "Enter the target population for the study");
+            }
+        }
+
+        if (model.Step == 7)
+        {
+            ValidateRecruitmentDates(model);
+
+            if (ModelState["RecruitmentStartDate.Day"].Errors.Count == 0 &&
+                ModelState["RecruitmentStartDate.Month"].Errors.Count == 0 &&
+                ModelState["RecruitmentStartDate.Year"].Errors.Count == 0 &&
+                ModelState["RecruitmentEndDate.Day"].Errors.Count == 0 &&
+                ModelState["RecruitmentEndDate.Month"].Errors.Count == 0 &&
+                ModelState["RecruitmentEndDate.Year"].Errors.Count == 0)
+            {
+                DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+                if (model.RecruitmentEndDate.ToDateOnly() < today)
+                {
+                    ModelState.AddModelError("RecruitmentEndDate.Day", "Recruitment end date (UK) must be in the future");
+                }
+
+                if (model.RecruitmentStartDate.ToDateOnly() > model.RecruitmentEndDate.ToDateOnly())
+                {
+                    ModelState.AddModelError("RecruitmentEndDate.Day", "Recruitment end date (UK) must be the same as or after Recruitment start date (UK)");
+                }
+            }
+
+            if (model.RecruitingIdentifiableVolunteers == null)
+            {
+                ModelState.AddModelError("RecruitingIdentifiableVolunteers", "Select whether participants in the study will be recruited as named individual volunteers");
+            }
+        }
     }
 
     public IActionResult AddStudySuccess(AddStudySuccessViewModel viewModel)
