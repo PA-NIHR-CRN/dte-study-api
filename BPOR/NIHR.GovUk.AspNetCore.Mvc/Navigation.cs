5afa6e9cfdeda6ad9f63c7294c4b3de273d914e8
Chris O'Neill <chris.oneill@nihr.ac.uk>
2024-07-22 10:03:01 +0100

    Subdivide master layout for NIHR.GovUk.AspNetCore.Mvc

---
+++ BPOR/NIHR.GovUk.AspNetCore.Mvc/Navigation.cs
@@ -0,0 +1,23 @@
+﻿namespace NIHR.GovUk
+{
+    public class Navigation
+    {
+        public IList<NavigationItem> Items { get; set; } = [];
+
+        public void Add(string label, Uri uri)
+        {
+            Items.Add(new NavigationItem { Label = label, Uri = uri });
+        }
+        
+        public void Add(NavigationItem item)
+        {
+            Items.Add(item);
+        }
+    }
+
+    public class NavigationItem
+    {
+        public string Label { get; set; } = "#";
+        public Uri Uri { get; set; } = new Uri("#", UriKind.Relative);
+    }
+}
