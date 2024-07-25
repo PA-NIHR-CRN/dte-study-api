aa884f453ae6d222941ed3cd1c82b39b96fa0954
peter.corrie <peter.corrie@nihr.ac.uk>
2024-07-25 13:42:36 +0100

    Remove unused using

--- BPOR/NIHR.GovUk.AspNetCore.Mvc/TagHelpers/ErrorSummaryTagHelper.cs
+++ BPOR/NIHR.GovUk.AspNetCore.Mvc/TagHelpers/ErrorSummaryTagHelper.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
