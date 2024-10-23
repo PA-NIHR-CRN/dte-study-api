52e32551e56806d56ab42b4038bc3bf8144825e6
Thomas Kirkwood <thomas.kirkwood@paconsulting.com>
2024-10-23 11:49:03 +0100

    load participant contact method with participant

--- BPOR/BPOR.Domain/Entities/ParticipantDbContext.cs
+++ BPOR/BPOR.Domain/Entities/ParticipantDbContext.cs
@@ -66,7 +66,7 @@ public class ParticipantDbContext : DbContext
     {
         var values = identifiers.Select(id => id.Value).ToList();
 
-        return Participants.Where(p => p.ParticipantIdentifiers.Any(pi => values.Contains(pi.Value)));
+        return Participants.Where(p => p.ParticipantIdentifiers.Any(pi => values.Contains(pi.Value))).Include(x => x.participantContactMethod);
     }
 
     public void ThrowIfInMaintenanceMode()
