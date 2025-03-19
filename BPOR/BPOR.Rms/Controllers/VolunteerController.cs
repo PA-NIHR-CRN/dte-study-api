70dccd68a6ee05f88df595a94d3345157030514a
kevinofficer <57766007+kevinofficer@users.noreply.github.com>
2025-03-19 15:42:53 +0000

    update case

--- BPOR/BPOR.Rms/Controllers/VolunteerController.cs
+++ BPOR/BPOR.Rms/Controllers/VolunteerController.cs
@@ -47,7 +47,7 @@ public class VolunteerController(ParticipantDbContext context,
         return View(new VolunteerFormViewModel());
     }
 
-    private void clearAllErrorsExcept(string errorToNotClear)
+    private void ClearAllErrorsExcept(string errorToNotClear)
     {
         foreach (var key in ModelState.Keys)
         {
@@ -68,7 +68,7 @@ public class VolunteerController(ParticipantDbContext context,
         if (action == "AddressLookup")
         {
 
-            clearAllErrorsExcept("PostCode");
+            ClearAllErrorsExcept("PostCode");
             if (!model.PostCode.HasValue)
             {
                     ModelState.AddModelError("PostCode", "Enter a full UK postcode");
