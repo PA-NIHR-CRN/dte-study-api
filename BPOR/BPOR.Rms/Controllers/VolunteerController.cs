1a785a57650212de75a8fcc2104f83c792ff538e
kevinofficer <57766007+kevinofficer@users.noreply.github.com>
2025-03-24 10:52:39 +0000

    fix: resolve postcode error
    its associated to address-lookup action and is not mapped correctly to the postcode field.

--- BPOR/BPOR.Rms/Controllers/VolunteerController.cs
+++ BPOR/BPOR.Rms/Controllers/VolunteerController.cs
@@ -63,7 +63,6 @@ public class VolunteerController(ParticipantDbContext context,
 
         if (action == "AddressLookup")
         {
-
             ClearAllErrorsExcept("PostCode");
             if (!model.PostCode.HasValue)
             {
@@ -75,6 +74,7 @@ public class VolunteerController(ParticipantDbContext context,
                 model.ManualAddressEntry = false;
             }
             model.SelectedAddress = null;
+            ModelState.AddModelError("PostCode", "We cannot find a match for the postcode entered. Please try again or enter your address manually.");
         }
 
         if (action == "ManualAddress")
