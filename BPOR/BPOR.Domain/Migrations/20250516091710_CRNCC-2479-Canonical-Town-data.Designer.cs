90b2a0996101ff88a7f3050db0d32b8e316e412e
thomas kirkwood <thomas.kirkwood@paconsulting.com>
2025-05-16 10:17:55 +0100

    added migration

--- BPOR/BPOR.Domain/Migrations/20250516091710_CRNCC-2479-Canonical-Town-data.Designer.cs
+++ BPOR/BPOR.Domain/Migrations/20250516091710_CRNCC-2479-Canonical-Town-data.Designer.cs
@@ -4,6 +4,7 @@ using BPOR.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
@@ -12,9 +13,11 @@ using NetTopologySuite.Geometries;
 namespace Dynamo.Stream.Handler.Migrations
 {
     [DbContext(typeof(ParticipantDbContext))]
-    partial class ParticipantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250516091710_CRNCC-2479-Canonical-Town-data")]
+    partial class CRNCC2479CanonicalTowndata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BPOR/BPOR.Domain/Migrations/20250516091710_CRNCC-2479-Canonical-Town-data.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dynamo.Stream.Handler.Migrations
+{
+    /// <inheritdoc />
+    public partial class CRNCC2479CanonicalTowndata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CanonicalTown",
+                table: "ParticipantAddress",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanonicalTown",
+                table: "ParticipantAddress");
+        }
+    }
+}
