8dfa0caa9310a360430b511745ad1fe8fc1eb81a
kevinofficer <57766007+kevinofficer@users.noreply.github.com>
2025-06-06 11:53:48 +0100

    logging at program run async

--- BPOR/DynamoDBupdate/Program.cs
+++ BPOR/DynamoDBupdate/Program.cs
@@ -9,15 +9,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddNihrConfiguration(builder.Services, builder.Environment);
 builder.Services.RegisterServices(builder.Configuration, builder.Environment);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 var app = builder.Build();
 
 var cts = new CancellationTokenSource();
 
 using (var scope = app.Services.CreateScope())
 {
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+    logger.LogInformation("Starting backfill RunAsync call.");
+
     var backfill = scope.ServiceProvider.GetRequiredService<Backfill>();
 
     await backfill.RunAsync(true, true, cts.Token);
+
+    logger.LogInformation("Completed backfill RunAsync call.");
 }
 
 app.Run();
