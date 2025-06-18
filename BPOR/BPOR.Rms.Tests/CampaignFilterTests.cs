d56d12e37cbe952b314c72d9e6ac583ea5e32405
kira.clarke <kira.clarke@nihr.ac.uk>
2025-06-18 14:03:21 +0100

    Initial implementation - Still needs unit tests.

---
+++ BPOR/BPOR.Rms.Tests/CampaignFilterTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPOR.Rms.Tests
+{
+    public class CampaignFilterTests
+    {
+        [Fact]
+        public async Task BatchedSampleSameAsMonolithic()
+        {
+
+        }
+
+    }
+}
