a380310756695cc84fbf078f6be05352c12ab0f4
kira.clarke <kira.clarke@nihr.ac.uk>
2025-06-26 15:07:27 +0100

    Addec a couple of missing tests; added option to enable MySQL tests.

---
+++ BPOR/BPOR.Rms.Tests/DatabaseFixture.cs
@@ -0,0 +1,29 @@
+﻿using BPOR.Registration.Stream.Handler.Tests;
+using BPOR.Tests.Common;
+
+namespace BPOR.Rms.Tests
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public DatabaseFixture()
+        {
+            var configuration = TestConfiguration.GetStandardConfiguration();
+            this.LocalParticipantDatabase = new LocalParticipantDatabase(configuration);
+
+            using var context = LocalParticipantDatabase.CreateDbContext();
+            var faker = new ParticipantFaker();
+            faker.UseSeed(5832);
+            var participants = faker.Generate(10000);
+            context.Participants.AddRange(participants);
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            LocalParticipantDatabase.Dispose();
+        }
+
+        public LocalParticipantDatabase LocalParticipantDatabase { get; set; }
+
+    }
+}
