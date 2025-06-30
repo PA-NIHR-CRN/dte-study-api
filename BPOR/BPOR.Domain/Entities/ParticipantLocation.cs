777397f7a50281ceb8b1ceb1f3ec1f6dab5a2d6e
kira.clarke <kira.clarke@nihr.ac.uk>
2025-06-30 12:47:34 +0100

    Fixed rebase merge mistake.

--- BPOR/BPOR.Domain/Entities/ParticipantLocation.cs
+++ BPOR/BPOR.Domain/Entities/ParticipantLocation.cs
@@ -9,12 +9,12 @@ namespace BPOR.Domain.Entities;
 public class ParticipantLocation : ISoftDelete, ITimestamped, IPersonalInformation
 {
     public int Id { get; set; }
-    public Point Location { get; set; }
-    public bool IsApproximate { get; set; }
+    public Point Location { get; set; } = Point.Empty;
+    public bool IsApproximate { get; set; } = false;
     public bool IsDeleted { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
-    public int ParticipantId { get; set; } 
+    public int ParticipantId { get; set; }
     public Participant Participant { get; set; }
     /// <summary>
     /// OSGB 6 digit Easting
