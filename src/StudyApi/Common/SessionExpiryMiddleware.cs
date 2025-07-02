f8898a01a8805c51ec1e158125537734e66dad75
Chris O'Neill <chris.oneill@nihr.ac.uk>
2025-07-02 21:33:08 +0100

    CRNCC-2406 - Inject logger into SessionExpiryMiddleware constructor.

--- src/StudyApi/Common/SessionExpiryMiddleware.cs
+++ src/StudyApi/Common/SessionExpiryMiddleware.cs
@@ -15,20 +15,22 @@ namespace StudyApi.Common;
 public class SessionExpiryMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly ILogger<SessionExpiryMiddleware> _logger;
 
-    public SessionExpiryMiddleware(RequestDelegate next)
+    public SessionExpiryMiddleware(RequestDelegate next, ILogger<SessionExpiryMiddleware> logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
-        var logger = context.RequestServices.GetRequiredService<ILogger<SessionExpiryMiddleware>>();
-
         var result = await context.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
         if (!result.Succeeded)
         {
+            _logger.LogWarning(result.Failure, result.Failure.Message);
+
             await _next(context);
             return;
         }
@@ -37,13 +39,13 @@ public class SessionExpiryMiddleware
         var sessionId = result.Principal.FindFirstValue("sessionId");
         var participantId = context.User.GetParticipantId();
 
-        var scopeData = new Dictionary<string, object>
+        var scopeData = new Dictionary<string, string>
         {
             ["sessionId"] = sessionId,
             ["participantId"] = participantId
         };
 
-        using (logger.BeginScope(scopeData))
+        using (_logger.BeginScope(scopeData))
         {
             var token = result.Principal.Claims;
             var nhsNumber = token?.FirstOrDefault(x => x.Type == "nhs_number")?.Value;
