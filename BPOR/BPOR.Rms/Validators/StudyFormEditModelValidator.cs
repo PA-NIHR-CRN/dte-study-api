8186518d40068dfbda140d4026d94c83e16c5869
kira.clarke <kira.clarke@nihr.ac.uk>
2025-07-21 14:31:03 +0100

    CRNCC-2668 Added missing length checks

--- BPOR/BPOR.Rms/Validators/StudyFormEditModelValidator.cs
+++ BPOR/BPOR.Rms/Validators/StudyFormEditModelValidator.cs
@@ -9,10 +9,12 @@ public class StudyFormModelValidator : AbstractValidator<StudyFormEditModel>
     public StudyFormModelValidator()
     {
         RuleFor(i => i.FullName)
-            .NotEmpty().WithMessage("Enter the name of the main contact for the study");
+            .NotEmpty().WithMessage("Enter the name of the main contact for the study")
+            .MaximumLength(255).WithMessage("Main contact name must be less than 256 characters");
         RuleFor(i => i.EmailAddress)
             .NotEmpty().WithMessage("Enter the email address of the main contact for the study")
-            .EmailAddress().WithMessage("Enter an email address in the correct format, like name@example.com");
+            .EmailAddress().WithMessage("Enter an email address in the correct format, like name@example.com")
+            .MaximumLength(255).WithMessage("Email address must be less than 256 characters");
         RuleFor(i => i.StudyName)
             .NotEmpty().WithMessage("Enter the study name")
             .MaximumLength(255).WithMessage("Study name must be less than 256 characters");
@@ -20,6 +22,7 @@ public class StudyFormModelValidator : AbstractValidator<StudyFormEditModel>
             .NotNull().WithMessage("Select whether the study is recruiting identifiable participants")
             .When(i => i.AllowEditIsRecruitingIdentifiableParticipants);
         RuleFor(i => i.InformationUrl)
-            .UriOrNullOrWhitespace().WithMessage("The website you have tried to enter is not formatted correct");
+            .UriOrNullOrWhitespace().WithMessage("The website you have tried to enter is not formatted correct")
+            .MaximumLength(2048).WithMessage("Website must be less than 2049 characters");
     }
 }
