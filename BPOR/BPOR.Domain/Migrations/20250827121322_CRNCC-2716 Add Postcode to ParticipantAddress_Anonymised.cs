fb61f0fb1297da0305eba64724858fa26b838e59
Chris O'Neill <chris.oneill@nihr.ac.uk>
2025-08-27 13:18:53 +0100

    CRNCC-2716 - Expose full postcode to D&A reporting

---
+++ BPOR/BPOR.Domain/Migrations/20250827121322_CRNCC-2716 Add Postcode to ParticipantAddress_Anonymised.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dynamo.Stream.Handler.Migrations
+{
+    /// <inheritdoc />
+    public partial class CRNCC2716AddPostcodetoParticipantAddress_Anonymised : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR REPLACE VIEW ParticipantAddress_Anonymised AS
+                SELECT pa.Id,
+                pa.CanonicalTown AS Town,
+                LEFT(TRIM(pa.Postcode), CHAR_LENGTH(TRIM(pa.Postcode)) - 3) AS Outcode, 
+                pa.Postcode
+                pa.ParticipantId,
+                pa.IsDeleted
+                FROM ParticipantAddress pa;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR REPLACE VIEW ParticipantAddress_Anonymised AS
+                SELECT pa.Id,
+                pa.CanonicalTown AS Town,
+                LEFT(TRIM(pa.Postcode), CHAR_LENGTH(TRIM(pa.Postcode)) - 3) AS Outcode, 
+                pa.ParticipantId,
+                pa.IsDeleted
+                FROM ParticipantAddress pa;
+            ");
+        }
+    }
+}
