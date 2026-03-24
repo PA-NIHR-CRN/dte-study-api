70d83a2a5b5e0228931c240cc32f9be4a648a60d
kevinofficer <57766007+kevinofficer@users.noreply.github.com>
2026-03-24 17:08:55 +0000

    update view query to take lowest Id

--- BPOR/BPOR.Domain/Migrations/20250925140402_CRNCC-2596-Participant-origin-view.cs
+++ BPOR/BPOR.Domain/Migrations/20250925140402_CRNCC-2596-Participant-origin-view.cs
@@ -17,43 +17,36 @@ namespace Dynamo.Stream.Handler.Migrations
                     SQL SECURITY DEFINER
                 VIEW `dte`.`Participants_Origin` AS
                 SELECT
-                    x.Id,
-                    x.ParticipantId,
-                    x.FirstName,
-                    x.LastName,
-                    x.IdentifierTypeId,
-                    x.Description,
-                    x.CreatedAt
-                FROM (
-                    SELECT 
-                        pi.Id AS Id,
-                        pi.ParticipantId AS ParticipantId,
-                        p.FirstName AS FirstName,
-                        p.LastName AS LastName,
-                        pi.IdentifierTypeId AS IdentifierTypeId,
-                        CASE
-                            WHEN pi.IdentifierTypeId = 1 THEN 'BPOR'
-                            WHEN pi.IdentifierTypeId = 2 THEN 'NHS'
-                            ELSE it.Description
-                        END AS Description,
-                        p.CreatedAt AS CreatedAt,
+                    pi.Id,
+                    pi.ParticipantId,
+                    p.FirstName,
+                    p.LastName,
+                    pi.IdentifierTypeId,
+                    CASE
+                        WHEN pi.IdentifierTypeId = 1 THEN 'BPOR'
+                        WHEN pi.IdentifierTypeId = 2 THEN 'NHS'
+                        ELSE it.Description
+                    END AS Description,
+                    p.CreatedAt
+                FROM dte.ParticipantIdentifiers pi
 
-                        ROW_NUMBER() OVER (
-                            PARTITION BY pi.ParticipantId
-                            ORDER BY pi.Id ASC
-                        ) AS rn
+                JOIN (
+                    SELECT ParticipantId, MIN(Id) AS MinId
+                    FROM dte.ParticipantIdentifiers
+                    WHERE IsDeleted = 0
+                    GROUP BY ParticipantId
+                ) first_pi
+                    ON pi.ParticipantId = first_pi.ParticipantId
+                AND pi.Id = first_pi.MinId
 
-                    FROM dte.ParticipantIdentifiers pi
-                    JOIN dte.SysRefIdentifierType it 
-                        ON pi.IdentifierTypeId = it.Id
-                    JOIN dte.Participants p 
-                        ON pi.ParticipantId = p.Id
-                        
-                    WHERE pi.Active = 1
-                    AND p.Active = 1
+                JOIN dte.Participants p 
+                    ON pi.ParticipantId = p.Id
 
-                ) x
-                WHERE x.rn = 1;
+                JOIN dte.SysRefIdentifierType it 
+                    ON pi.IdentifierTypeId = it.Id
+
+                WHERE pi.IsDeleted = 0
+                AND p.IsDeleted = 0;
                 ");
         }
 
