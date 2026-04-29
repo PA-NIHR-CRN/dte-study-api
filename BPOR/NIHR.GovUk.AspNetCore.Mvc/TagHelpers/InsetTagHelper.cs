c08d218c1d4bc0c49231d9440e2a501b49c76731
kira.clarke <kira.clarke@nihr.ac.uk>
2026-04-29 19:15:09 +0100

    CRNCC-2991 Bug fixes

---
+++ BPOR/NIHR.GovUk.AspNetCore.Mvc/TagHelpers/InsetTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using NIHR.GovUk.AspNetCore.Mvc.Models;
+using NIHR.GovUk.AspNetCore.Mvc.Views;
+
+namespace NIHR.GovUk.AspNetCore.Mvc.TagHelpers;
+
+public class InsetTextTagHelper(IHtmlHelper htmlHelper) : PartialTagHelperBase(htmlHelper)
+{
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {        
+        output.TagName = "div";
+        output.AddClass("govuk-inset-text", HtmlEncoder.Default);
+    }
+}
