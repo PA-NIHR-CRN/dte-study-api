1ad76f1cd0b80292bb58ea1e275c0d673ba99f1e
kira.clarke <kira.clarke@nihr.ac.uk>
2026-05-19 21:00:14 +0100

    DIGENG-788 Add test API method

---
+++ BPOR/BPOR.Rms.Api/Controllers/VolunteerController.cs
@@ -0,0 +1,22 @@
+﻿using BPOR.Rms.Api.Models.Volunteer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPOR.Rms.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VolunteerController : ControllerBase
+{
+    [HttpGet("information/{token}")]
+    public async Task<ActionResult<GetInformationResponse>> GetInformation(string token, CancellationToken cancellationToken)
+    {
+        var result = new GetInformationResponse
+        {
+            ParticipantEmail = "test@test-domain.test",
+            ParticipantId = 123,
+            ParticipantName = "Alice Smith"
+        };
+        
+        return Ok(result);
+    }
+}
---
+++ BPOR/BPOR.Rms.Api/Models/Volunteer/GetInformationResponse.cs
@@ -0,0 +1,8 @@
+namespace BPOR.Rms.Api.Models.Volunteer;
+
+public class GetInformationResponse
+{
+    public long ParticipantId { get; set; }
+    public string ParticipantName { get; set; }
+    public string ParticipantEmail { get; set; }
+}
