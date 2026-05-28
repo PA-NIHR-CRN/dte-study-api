86f7db8e14c9fead27af67d7e629b571712f814a
kira.clarke <kira.clarke@nihr.ac.uk>
2026-05-28 10:39:45 +0100

    CRNCC-3085

---
+++ BPOR/BPOR.Domain/Entities/Configuration/CampaignParticipantConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BPOR.Domain.Entities.Configuration;
+
+public class CampaignParticipantConfiguration : IEntityTypeConfiguration<CampaignParticipant>
+{
+    public void Configure(EntityTypeBuilder<CampaignParticipant> builder)
+    {
+        builder.Property(i => i.Token).HasMaxLength(64);
+    }
+}
