83d3b34b0854d90bd69f0e5af66527f2e0b5aa28
kira.clarke <kira.clarke@nihr.ac.uk>
2026-05-28 10:43:24 +0100

    CRNCC-3085

---
+++ BPOR/BPOR.Domain/Migrations/20260528064006_CRNCC-3085-AddTokenColumn.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BPOR.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CRNCC3085AddTokenColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "CampaignParticipant",
+                type: "varchar(32)",
+                maxLength: 32,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "CampaignParticipant");
+        }
+    }
+}
