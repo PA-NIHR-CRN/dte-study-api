91ab7aa0b27bd238a17e81dd94f53041bef36490
kira.clarke <kira.clarke@nihr.ac.uk>
2026-06-18 11:41:44 +0100

    CRNCC-3072 Prescreener Mockup

---
+++ BPOR/BPOR.PrescreenerMock/Controllers/RecruitRegistryVolunteersController.cs
@@ -0,0 +1,17 @@
+﻿using BPOR.PrescreenerMock.Models;
+using BPOR.Rms.ApiClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPOR.PrescreenerMock.Controllers;
+
+[Route("Recruit-Registry-Volunteers")]
+public class RecruitRegistryVolunteersController(
+    IRmsApiClient apiClient, ILogger<RecruitRegistryVolunteersController> logger): Controller
+{
+    [HttpGet("volunteer/check-research")]
+    public async Task<IActionResult> Start(string token, string id, CancellationToken cancellationToken)
+    {
+        var info = await apiClient.GetInformation(token, cancellationToken);
+        return View(new StartModel(info, id));
+    }
+}
