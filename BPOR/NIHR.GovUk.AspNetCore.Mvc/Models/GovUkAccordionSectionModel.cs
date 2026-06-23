d4dbb9739fff8ee2be40c89f854f1bae3a1a84b3
kira.clarke <kira.clarke@nihr.ac.uk>
2026-06-23 14:38:29 +0100

    CRNCC-3086 Made tables collapsible, fixed date format.

---
+++ BPOR/NIHR.GovUk.AspNetCore.Mvc/Models/GovUkAccordionSectionModel.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NIHR.GovUk.AspNetCore.Mvc.Models;
+
+public record GovUkAccordionSectionModel (
+    string Caption,
+    TagHelperContent InnerContent) : GovUkModelWithContent(InnerContent);
