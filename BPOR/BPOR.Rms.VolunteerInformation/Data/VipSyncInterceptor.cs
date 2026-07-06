d2f12d22e71bcb1591ce799137fabd68c5e66b56
kira.clarke <kira.clarke@nihr.ac.uk>
2026-07-06 08:10:28 +0100

    CRNCC-3161

--- BPOR/BPOR.Rms.VolunteerInformation/Data/VipSyncInterceptor.cs
+++ BPOR/BPOR.Rms.VolunteerInformation/Data/VipSyncInterceptor.cs
@@ -1,5 +1,6 @@
 ﻿using BPOR.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace BPOR.Rms.VolunteerInformation.Data;
@@ -14,12 +15,27 @@ public class VipSyncInterceptor(IVipRepository vipRepository) : ISaveChangesInte
         CancellationToken cancellationToken)
     {
         var studies = eventData.Context.ChangeTracker.Entries<Study>();
-        foreach (var study in studies.Where(s => s.State == EntityState.Modified))
+        foreach (var study in studies.Where(s => s.State == EntityState.Modified
+                 && HaveAnyChanged(s, nameof(Study.StudyName), nameof(Study.RecruitmentEndDate))))
         {
             _updatedRecords.Add(new StudyValues(study.Entity.Id, study.Entity.StudyName, study.Entity.RecruitmentEndDate));
         }
 
         return ValueTask.FromResult(result);
+    }
+
+    private bool HaveAnyChanged(EntityEntry entry, params string[] propertyNames)
+    {
+        foreach (var propertyName in propertyNames)
+        {
+            var propertyEntry = entry.Property(propertyName);
+            if (propertyEntry?.IsModified == true)
+            {
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     public async ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, int result,
