77ec556f8b5a8146485e37d095a30e1c60d26353
kira.clarke <kira.clarke@nihr.ac.uk>
2026-07-06 12:15:45 +0100

    CRNCC-3140

--- BPOR/BPOR.Rms/Startup/DependencyInjection.cs
+++ BPOR/BPOR.Rms/Startup/DependencyInjection.cs
@@ -38,7 +38,8 @@ public static class DependencyInjection
         services.AddKeyedServiceDictionary();
         
         services.AddSingleton(TimeProvider.System);
-
+        services.AddSingleton<ITimezoneProvider, UkTimezoneProvider>();
+        
         services.AddControllersWithViews().AddRazorRuntimeCompilation();
         services.AddHttpContextAccessor();
 
---
+++ BPOR/NIHR.Infrastructure/Services/UkTimezoneProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NIHR.Infrastructure.Interfaces;
+
+namespace NIHR.Infrastructure.Services;
+
+public class UkTimezoneProvider : ITimezoneProvider
+{
+    public TimeZoneInfo GetCurrentTimezone()
+    {
+        return TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+    }
+}
