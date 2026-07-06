13a5f9321eceb93ac3e23cde2a7b405827535d3f
kira.clarke <kira.clarke@nihr.ac.uk>
2026-07-06 12:33:54 +0100

    CRNCC-3140

--- BPOR/NIHR.Infrastructure/Services/UkTimezoneProvider.cs
+++ BPOR/NIHR.Infrastructure/Services/UkTimezoneProvider.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Linq;
 using NIHR.Infrastructure.Interfaces;
 
 namespace NIHR.Infrastructure.Services;
 
 public class UkTimezoneProvider : ITimezoneProvider
 {
+    private TimeZoneInfo _timezone;
+    
+    public UkTimezoneProvider()
+    {
+        string[] desiredTimezoneNames = ["GMT Standard Time", "Europe/London"];
+        var availableTimeZones = TimeZoneInfo.GetSystemTimeZones();
+        _timezone = availableTimeZones.FirstOrDefault(i => desiredTimezoneNames.Contains(i.StandardName)) ?? TimeZoneInfo.Utc;
+    }
+
     public TimeZoneInfo GetCurrentTimezone()
     {
-        return TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+        return _timezone;
     }
 }
