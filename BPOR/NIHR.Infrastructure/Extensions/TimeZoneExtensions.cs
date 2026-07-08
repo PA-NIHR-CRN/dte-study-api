0bd70b3992bfae6078d0c6edfeba1ac63873a4b0
kira.clarke <kira.clarke@nihr.ac.uk>
2026-07-08 11:20:38 +0100

    CRNCC-3140

---
+++ BPOR/NIHR.Infrastructure/Extensions/TimeZoneExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using NIHR.Infrastructure.Interfaces;
+
+namespace NIHR.Infrastructure;
+
+public static class TimeZoneExtensions
+{
+    public static DateTime UtcToLocalTime(this ITimezoneProvider timezoneProvider, DateTime utc) 
+        => TimeZoneInfo.ConvertTimeFromUtc(utc, timezoneProvider.GetCurrentTimezone());
+}
