720311d9f688e8002d24135fb64c5d6bb84af2c4
kevinofficer <57766007+kevinofficer@users.noreply.github.com>
2026-07-31 14:37:16 +0100

    CRNCC-3199: revert stream handler from main, its deployed from rms

--- Dynamo.Stream.Handler/src/Dynamo.Stream.Handler/Entities/Participant.cs
+++ Dynamo.Stream.Handler/src/Dynamo.Stream.Handler/Entities/Participant.cs
@@ -72,9 +72,8 @@ public class Participant : ISoftDelete, ITimestamped, IPersonalInformation
         LandlineNumber = null;
         RegistrationConsent = false;
         RemovalOfConsentRegistrationAtUtc = DateTime.UtcNow;
-        NHSNumber = null;
         HasLongTermCondition = null; // TODO: confirm we are clearing this but not DailyLifeImpact?
         Address?.Anonymise();
-        HealthConditions?.Clear();
+        HealthConditions.Clear();
     }
 }
