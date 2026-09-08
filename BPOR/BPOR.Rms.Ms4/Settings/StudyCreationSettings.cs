683f749701ac07365737445c3e325eff8d17d505
kira.clarke <kira.clarke@nihr.ac.uk>
2026-09-08 13:39:48 +0100

    CRNCC-3214 Make post-application redirect URL configurable.

---
+++ BPOR/BPOR.Rms.Ms4/Settings/StudyCreationSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace BPOR.Rms.Ms4.Settings;
+
+public class StudyCreationSettings
+{
+    public string ResearcherCreationFinishRedirect { get; set; } = "https://bepartofresearch.nihr.ac.uk/";
+}
